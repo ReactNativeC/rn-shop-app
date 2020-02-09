5f73123a30a7e7edb602f52b51c471d46d834e73
Noor <noormahammad@users.noreply.github.com>
2020-02-09 15:11:30 -0500

    adding orderReducer to app.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStore, combineReducers, compose} from 'redux';
 import { Provider } from 'react-redux';
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
 import ShopNavigator from './navigation/ShopNavigator';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
@@ -15,6 +16,7 @@ import * as Font from 'expo-font';
 const rootReducer = combineReducers({
   products: productsReducer, 
   cart: cartReducer,  
+  order: ordersReducer,
 })
 
 
