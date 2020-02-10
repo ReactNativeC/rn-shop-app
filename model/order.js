3d34396d088ff5424fff081471ceab1716b6478e
Noor <noormahammad@users.noreply.github.com>
2020-02-09 20:36:16 -0500

    using moment.js library to display user friendly dates both on android and iOS

--- model/order.js
+++ model/order.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 class order {
   constructor(id, cartItems, totalAmount, date) {
     this.id = id, 
@@ -6,14 +8,18 @@ class order {
     this.date = date
   }
   
-  get readableDate() {
-    return this.date.toLocaleDateString('en-En', {
-      year : 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
-    });
+  get readableDate() {    
+    //this does not work on android, hence use "moment" npm pakcage to display user friendly date
+    // return this.date.toLocaleDateString('en-En', {
+    //   year : 'numeric',
+    //   month: 'long',
+    //   day: 'numeric',
+    //   hour: '2-digit',
+    //   minute: '2-digit'
+    // });
+
+    return moment(this.date).format('MMMM Do YYYY, hh:mm') // February 9th 2020 08:35
+    //return moment(this.date, 'MMMM Do YYYY, hh:mm').fromNow(); // few seconds ago // 30 minutes ago // 6 hours ago //etc//
   }
 }
 
