86beb76e414b2c403707463a038a4e5955dd1d38
Noor <noormahammad@users.noreply.github.com>
2020-02-15 19:17:17 -0500

    useReducer for efficient state management on the form (thus avoiding usage of lot of useState variables)

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,62 +1,105 @@
-import React, { useState, useEffect, useCa, useCallback } from 'react';
-import { View, Text, Button, TextInput, StyleSheet, ScrollView, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform, Alert} from 'react-native';
+import React, { useEffect, useCallback, useReducer } from 'react';
+import { View, Text, TextInput, StyleSheet, ScrollView, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform, Alert} from 'react-native';
 import { useDispatch, useSelector  } from 'react-redux';
 import * as productActions from '../../store/actions/products';
-import * as cartActions from '../../store/actions/cart';
-import Colors from '../../constants/colors';
 import Product from '../../model/product';
 import HeaderButton  from '../../components/UI/HeaderButton';
 import { HeaderButtons, Item} from 'react-navigation-header-buttons';
+
+const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
+
+const formReducer = (state, action) => {
+  switch(action.type){
+    case FORM_INPUT_UPDATE:      
+      const updatedValues = {
+        ...state.inputValues,
+        [action.input]: action.value
+      };
+
+      const updatedValidities = {
+        ...state.inputValidities, 
+        [action.input]: action.isValid
+      };
+
+      let updatedFormIsValid = true; 
+      for(const key in updatedValidities)
+        updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+
+      return {
+        inputValues: updatedValues, 
+        inputValidities: updatedValidities, 
+        formIsValid: updatedFormIsValid
+      };
+  }
+  
+  return state;
+};
 
 const EditProductScreen = (props) => {
   const dispatch = useDispatch();
   const productId = props.navigation.getParam("productId");
   const editedProduct = useSelector(state=> state.products.availableProducts.find(product => product.id === productId)); 
  
-  const [title, setTitle] = useState(editedProduct? editedProduct.title: '');
-  const [imageUrl, setImageUrl] =  useState(editedProduct? editedProduct.imageUrl: '');
-  const [price, setPrice] = useState(editedProduct? editedProduct.price: '');
-  const [description, setDescription] = useState(editedProduct? editedProduct.description: '');
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      title: editedProduct ? editedProduct.title : '',
+      imageUrl: editedProduct ? editedProduct.imageUrl : '',
+      price: editedProduct ? editedProduct.price : '',
+      description: editedProduct ? editedProduct.description : ''
+    },
+    inputValidities: {
+      title: editedProduct ? true : false,
+      imageUrl: editedProduct ? true : false,
+      price: editedProduct ? true : false,
+      description: editedProduct ? true : false,
+    },
+    formIsValid: editedProduct ? true : false
+  })
   
-  const [titleIsValid, setTitleIsValid] = useState(false);
-  const [priceIsValid, setPriceIsValid] = useState(false);
-  
-  const titleChangeHandler = text => {
-    if(text.length < 2)
-      setTitleIsValid(false);
-    else
-      setTitleIsValid(true)
-    
-      setTitle(text);    
-  }
-
-  const priceChangeHandler = price => {
+  const textChangeHandler = (inputIdentifier, text) => {
+    let isValid = false;    
+    if(text.trim().length > 0) {
+      isValid = true;
+    }
    
-    if(parseFloat(price) < 1.0)
-      setPriceIsValid(false);
-    else
-      setPriceIsValid(true)
-    
-      setPrice(price);   
+    dispatchFormState({
+      type: FORM_INPUT_UPDATE, 
+      value: text, 
+      isValid: isValid, 
+      input: inputIdentifier
+    })   
   }
 
   const submitHandler = useCallback(() => {
-    if(title.length < 2 || price < 1)
+    if(!formState.formIsValid)
     {
-      Alert.alert("Invalid", "Please correct all issues before proceeding!",[{title:"OK"} ])
+      Alert.alert("Wrong Input", "Please correct issues on the screen!",[{title:"OK"} ])
       return;
     }
+
     editedProduct ?
-      dispatch(productActions.editProduct(new Product(editedProduct.id, editedProduct.ownerId, title, imageUrl, description, parseFloat(parseFloat(price).toFixed(2)))))
+      dispatch(productActions.editProduct(new Product(
+        editedProduct.id, 
+        editedProduct.ownerId, 
+        formState.inputValues.title, 
+        formState.inputValues.imageUrl, 
+        formState.inputValues.description, 
+        parseFloat(parseFloat(formState.inputValues.price).toFixed(2)))))
       :
-      dispatch(productActions.addProduct(new Product(1, "u1", title, imageUrl, description, parseFloat(parseFloat(price).toFixed(2)))))          
+      dispatch(productActions.addProduct(new Product(
+        1, 
+        "u1", 
+        formState.inputValues.title, 
+        formState.inputValues.imageUrl, 
+        formState.inputValues.description, 
+        parseFloat(parseFloat(formState.inputValues.price).toFixed(2)))))          
       props.navigation.goBack();
-  },[dispatch, editedProduct, title, imageUrl, description, price, titleIsValid, priceIsValid])
+  },[dispatch, editedProduct, formState])
 
   useEffect(() => {
     props.navigation.setParams({submitFunc: submitHandler});
-  }, [submitHandler])
-  
+  }, [submitHandler])    
+
   return (
     <ScrollView>
       <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={60}>
@@ -66,13 +109,13 @@ const EditProductScreen = (props) => {
               <Text style={styles.titleText}>Title</Text>
               <TextInput 
                 style={styles.input} 
-                value={title} 
-                onChangeText={titleChangeHandler}
+                value={formState.inputValues.title} 
+                onChangeText={textChangeHandler.bind(this, 'title')}
                 clearButtonMode="while-editing"      
                 autoCapitalize="sentences"                    
               />
               {
-                !titleIsValid && (
+                !formState.inputValidities.title && (
                   <Text style={{ color: 'maroon' }}>Please enter a valid Title</Text>
                 )
               }
@@ -81,8 +124,8 @@ const EditProductScreen = (props) => {
             <View style={styles.formControl}>
               <Text style={styles.titleText}>Image Url</Text>
               <TextInput style={styles.input} 
-                value={imageUrl} 
-                onChangeText={input=>setImageUrl(input)} 
+                value={formState.inputValues.imageUrl} 
+                onChangeText={textChangeHandler.bind(this, 'imageUrl')} 
                 clearButtonMode="while-editing" 
                 keyboardType=""
                 autoCapitalize="none"
@@ -93,12 +136,12 @@ const EditProductScreen = (props) => {
               <Text style={styles.titleText}>Price</Text>            
               <TextInput keyboardType="decimal-pad" 
                 style={styles.input} 
-                value={price.toString()} 
-                onChangeText={priceChangeHandler} 
+                value={formState.inputValues.price.toString()} 
+                onChangeText={textChangeHandler.bind(this, 'price')} 
                 clearButtonMode="while-editing"
               />
               {
-                !priceIsValid && (
+                !formState.inputValidities.price && (
                   <Text style={{ color: 'maroon' }}>Price must be great than $1 USD</Text>
                 )
               }
@@ -107,8 +150,8 @@ const EditProductScreen = (props) => {
             <View style={styles.formControl}>
               <Text style={styles.titleText}>Description</Text>
               <TextInput style={styles.input} 
-                value={description} 
-                onChangeText={input=>setDescription(input)}                           
+                value={formState.inputValues.description} 
+                onChangeText={textChangeHandler.bind(this, 'description')}                           
                 blurOnSubmit
                 clearButtonMode="while-editing"     
                 returnKeyType="done"                                                                     
