275d9bed27a9fe7b65bfe7e7f36bbc049218dbe7
Noor <noormahammad@users.noreply.github.com>
2020-02-16 13:22:15 -0500

    Storing products on server (Post request to Firebase)

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,6 +1,7 @@
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const ADD_PRODUCT = 'ADD_PRODUCT';
 export const EDIT_PRODUCT = 'EDIT_PRODUCT';
+import Config from '../../secrets/config';
 export const deleteProduct = id => {
   return {
     type: DELETE_PRODUCT, 
@@ -8,11 +9,30 @@ export const deleteProduct = id => {
   }
 }
 export const addProduct = product => {
-  return {
-    type: ADD_PRODUCT, 
-    product: product
-  }
+  return async  dispatch => {
+    //ansy async action here
+    const response = await fetch(Config.database +'/products.json', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',        
+      }, 
+      body: JSON.stringify({
+        title: product.title, 
+        description: product.description, 
+        imageUrl: product.imageUrl, 
+        price: product.price
+      })
+    })    
+    const resData = await response.json();
+    product.id = resData.name;
+    
+    dispatch({
+      type: ADD_PRODUCT, 
+      product: product
+    })
+  }  
 }
+
 export const editProduct = product => {
   return {
     type: EDIT_PRODUCT, 
